9776131bb4f4f97a1fee9a5031dd1fe605765dc4
agithyogendra <agithyogendra@gmail.com>
2017-01-29 09:02:02 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/a/todolist/MainActivity.java
+++ app/src/main/java/com/example/a/todolist/MainActivity.java
@@ -184,9 +184,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        // Handle action bar item clicks here
         int id = item.getItemId();
 
         //Clear button Activity
